6be99edfec8ed4893a219a2cf1fb7fe358fba6b4
Muratcan Gözüm <murat.can1250@gmail.com>
2024-05-10 12:27:09 +0300

    minor adjustment and push testing

--- app/src/main/java/com/fal/tutkusu/MainActivity.kt
+++ app/src/main/java/com/fal/tutkusu/MainActivity.kt
@@ -7,40 +7,45 @@ import android.view.View
 import com.fal.tutkusu.Falbaktir.FalMenu
 import com.fal.tutkusu.GunlukBurc.GunlukBurcMenu
 import com.fal.tutkusu.TarotFali.TarotFaliMenu
+import com.fal.tutkusu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-
-        // linearcard1 ID'sine sahip view'i buluyoruz
-        val linearCard1 = findViewById<View>(R.id.linearcard1)
-        val linearCard2 = findViewById<View>(R.id.linearcard2)
-        val linearCard3 = findViewById<View>(R.id.linearcard3)
-
         // onClickListener ekliyoruz
-        linearCard1.setOnClickListener {
+        binding.linearcard1.setOnClickListener {
             // FalMenu ekranına geçmek için bir Intent oluşturuyoruz
             val intent = Intent(this, FalMenu::class.java)
             // Intent'i başlatıyoruz
             startActivity(intent)
         }
-        linearCard2.setOnClickListener {
+
+        binding.linearcard2.setOnClickListener{
             // FalMenu ekranına geçmek için bir Intent oluşturuyoruz
             val intent = Intent(this, GunlukBurcMenu::class.java)
             // Intent'i başlatıyoruz
             startActivity(intent)
         }
-        linearCard3.setOnClickListener {
+
+        binding.linearcard3.setOnClickListener{
             // FalMenu ekranına geçmek için bir Intent oluşturuyoruz
             val intent = Intent(this, TarotFaliMenu::class.java)
             // Intent'i başlatıyoruz
             startActivity(intent)
         }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
 
-
-
+        _binding = null
     }
 }
