90e179e09e4a83d0982bb9e543d2420dc4078d95
OKAN IŞIK <okandeveloper@gmail.com>
2024-05-10 09:01:19 +0300

    Version01

---
+++ app/src/main/java/com/fal/tutkusu/Falbaktir/FalMenu.kt
@@ -0,0 +1,12 @@
+package com.fal.tutkusu.Falbaktir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fal.tutkusu.R
+
+class FalMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fal_menu)
+    }
+}
---
+++ app/src/main/java/com/fal/tutkusu/TarotFali/TarotFaliMenu.kt
@@ -0,0 +1,12 @@
+package com.fal.tutkusu.TarotFali
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fal.tutkusu.R
+
+class TarotFaliMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tarot_fali_menu)
+    }
+}
